d8dbcd9627e5b5341207ad4788a1d0fc133efefa
franciscoaerton <franciscoaerton@gmail.com>
2017-06-24 14:02:04 -0300

    Create joson_router.php

---
+++ joson_router.php
@@ -0,0 +1,16 @@
+<?php
+
+function json_router ($category, $var){
+switch ($i) {
+    case "apple":
+        echo "i is apple";
+        break;
+    case "bar":
+        echo "i is bar";
+        break;
+    case "cake":
+        echo "i is cake";
+        break;
+}
+}
+?>
