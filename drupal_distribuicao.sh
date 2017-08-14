354d7cbb3dd1571099626287f6c87e4ddb126437
franciscoaerton <franciscoaerton@gmail.com>
2017-08-14 14:44:34 -0300

    Update drupal_distribuicao.sh

--- drupal_distribuicao.sh
+++ drupal_distribuicao.sh
@@ -25,25 +25,12 @@ if [ "$is_confirmed" == 1 ]; then
 	sudo wget -c $path_distribuicao o- $local_da_pasta/web/distro.tar.gz;
 	sudo tar -c $local_da_pasta/web -xvzf $local_da_pasta/web/distro.tar.gz;
 	sudo mv $local_da_pasta/web/distro/* $local_da_pasta/web;
-	
-	
-	sudo tar -C /var/www/html/$nome_da_pasta/public_html -xvzf /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44-core.tar.gz;
-	
-	
-	sudo rm /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44-core.tar.gz;
-	sudo rm -rf ./panopoly-7.x-1.44;
-	sudo mv /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44/* /var/www/html/$nome_da_pasta/public_html/;
-	sudo mv /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44/* /var/www/html/$nome_da_pasta/public_html/;
-	sudo mv /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44/.htaccess /var/www/html/$nome_da_pasta/public_html/;
-	sudo mv /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44/.gitignore /var/www/html/$nome_da_pasta/public_html/;
-	sudo rm -rf /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44;
+	sudo mv $local_da_pasta/web/distro/.htaccess $local_da_pasta/web;
+		
 	sudo chown www-data:www-data -c -R /var/www/html/$nome_da_pasta/public_html/sites;
 	
-	#sudo nano /etc/apache2/sites-available/core.conf
-	#sudo a2ensite core.conf
 	
-	sudo service apache2 restart;
 
 else
-	echo "Nao houve criacao de pastas!";
+	echo "Nao houve movimentação de arquivos!";
 fi
