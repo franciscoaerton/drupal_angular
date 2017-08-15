69e4a0a3ab52806ed5007a2734ed78a9c3244499
franciscoaerton <franciscoaerton@gmail.com>
2017-08-14 22:22:28 -0300

    Update drupal_distribuicao.sh

--- drupal_distribuicao.sh
+++ drupal_distribuicao.sh
@@ -3,14 +3,10 @@
 echo "Informar o caminho pasta_site: " 
 read local_da_pasta;
 
-echo "Informar path do arquivo distribuicao_drupal: " 
-read path_distribuicao; 
-
 echo " 
 ====================================
 Dados para criacao: 
-Local da Pasta: '$local_da_pasta'
-Nome do site: '$path_distribuicao'
+Local da Pasta (sem web, na raiz web[x]): '$local_da_pasta'
 ====================================
 ";
 
@@ -18,12 +14,23 @@ echo "Confirme as informacoes - 1 'sim', outro valor 'nao': "
 read is_confirmed
 
 if [ "$is_confirmed" == 1 ]; then 
-	# baixo arquivos drupal de instalação
-	sudo wget -c $path_distribuicao -O $local_da_pasta/web/distro.tar.gz;
-	# descompacto a pasta distro
-	sudo tar -xvzf $local_da_pasta/web/distro.tar.gz;
-	# deleto todos arquivos tar.gz
-	sudo rm -rf $local_da_pasta/web/distro.tar.gz; 
+	# coloco o ponteiro no diretorio do site:
+	cd $local_da_pasta/web;
+	
+	# baixo arquivos drupal de instalação na pasta do site:
+	sudo wget -c https://ftp.drupal.org/files/projects/panopoly-7.x-1.46-core.tar.gz;
+	
+	# descompacto o arquivo para uma pasta e deleto o arquivo compactado:
+	sudo tar -xvzf panopoly-7.x-1.46-core.tar.gz;
+	sudo rm -rf panopoly-7.x-1.46-core.tar.gz;
+	
+	# Movimento os arquivos da instalação drupal um diretório acima:
+	sudo mv  panopoly-7.x-1.46/* $local_da_pasta/web;
+	sudo rm -rf panopoly-7.x-1.46; 
+	sudo rm -rf panopoly-7.x-1.44;
+	
+	#movo os arquivos um diretório acima
+	
 	
 	#sudo mv $local_da_pasta/web/distro/* $local_da_pasta/web;
 	#sudo mv $local_da_pasta/web/distro/.htaccess $local_da_pasta/web;
