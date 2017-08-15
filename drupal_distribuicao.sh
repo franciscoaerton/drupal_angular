86d2663ac4e893a2c5cf171c5fe91a2433e9ddfe
franciscoaerton <franciscoaerton@gmail.com>
2017-08-14 21:35:44 -0300

    versão testada hoje

--- drupal_distribuicao.sh
+++ drupal_distribuicao.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-# My First Script
 
 echo "Informar o caminho pasta_site: " 
 read local_da_pasta;
@@ -12,26 +11,24 @@ echo "
 Dados para criacao: 
 Local da Pasta: '$local_da_pasta'
 Nome do site: '$path_distribuicao'
-
-
-
 ====================================
 ";
 
 echo "Confirme as informacoes - 1 'sim', outro valor 'nao': "
 read is_confirmed
 
-if [ "$is_confirmed" == 1 ]; then
+if [ "$is_confirmed" == 1 ]; then 
 	# baixo arquivos drupal de instalação
-	sudo wget -c $path_distribuicao O- $local_da_pasta/web/distro.tar.gz;
+	sudo wget -c $path_distribuicao -O $local_da_pasta/web/distro.tar.gz;
 	# descompacto a pasta distro
-	sudo tar -c $local_da_pasta/web -xvzf $local_da_pasta/web/distro.tar.gz;
+	sudo tar -xvzf $local_da_pasta/web/distro.tar.gz;
 	# deleto todos arquivos tar.gz
-	sudo rm $local_da_pasta/web/*.tar.gz;
+	sudo rm -rf $local_da_pasta/web/distro.tar.gz; 
+	
 	#sudo mv $local_da_pasta/web/distro/* $local_da_pasta/web;
 	#sudo mv $local_da_pasta/web/distro/.htaccess $local_da_pasta/web;
 		
-	#sudo chown www-data:www-data -c -R /var/www/html/$nome_da_pasta/public_html/sites;
+	#sudo chown www-data:www-data -c -R /var/www/html/$nome_da_pasta/public_html/sites; 
 	
 	
 
