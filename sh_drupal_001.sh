90a6461483a7d104a0fc5bbe26b94d9f26bd5c16
franciscoaerton <franciscoaerton@gmail.com>
2017-12-05 12:39:03 -0200

    Create sh_drupal_001.sh

---
+++ sh_drupal_001.sh
@@ -0,0 +1,112 @@
+#! /bin/bash
+# My First Script
+
+echo "Informar o nome da nova pasta: " 
+read nome_da_pasta;
+
+echo "Informar o nome do site: " 
+read nome_do_site;
+
+echo " 
+====================================
+Dados para criacao: 
+Pasta a ser criada: '$nome_da_pasta'
+Nome do site: '$nome_do_site'
+====================================
+";
+
+echo "Confirma as informacoes - 1 'sim', outro valor 'nao': "
+read is_confirmed
+
+if [ "$is_confirmed" == 1 ]; then
+	sudo mkdir /var/www/html/$nome_da_pasta;
+	sudo mkdir /var/www/html/$nome_da_pasta/public_html;
+	sudo mkdir /var/www/html/$nome_da_pasta/log;
+	sudo mkdir /var/www/html/$nome_da_pasta/backups;
+	sudo wget -c -P /var/www/html/$nome_da_pasta/public_html https://ftp.drupal.org/files/projects/panopoly-7.x-1.44-core.tar.gz;
+	sudo tar -C /var/www/html/$nome_da_pasta/public_html -xvzf /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44-core.tar.gz;
+	sudo rm /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44-core.tar.gz;
+	sudo rm -rf ./panopoly-7.x-1.44;
+	sudo mv /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44/* /var/www/html/$nome_da_pasta/public_html/;
+	sudo mv /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44/* /var/www/html/$nome_da_pasta/public_html/;
+	sudo mv /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44/.htaccess /var/www/html/$nome_da_pasta/public_html/;
+	sudo mv /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44/.gitignore /var/www/html/$nome_da_pasta/public_html/;
+	sudo rm -rf /var/www/html/$nome_da_pasta/public_html/panopoly-7.x-1.44;
+	sudo chown www-data:www-data -c -R /var/www/html/$nome_da_pasta/public_html/sites;
+	
+	#sudo nano /etc/apache2/sites-available/core.conf
+	#sudo a2ensite core.conf
+	
+	sudo service apache2 restart;
+
+else
+	echo "Nao houve criacao de pastas!";
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###### arquivo bash #############
+#!/bin/bash
+NAME="$1"
+DIR="$2"
+
+tar -zcf $NAME.tar.gz $DIR
+
+#################################
+
+################### arquivo php #
+
+<?php
+shell_exec('sh seu_script.sh nome_qualquer diretorio_qualquer');
+?>
+
+#################################
+
+--
+
+#!/bin/bash
+
+    NAME=$1
+    DIR=$2
+
+    echo $NAME
+    echo $DIR
+    tar -zcf $NAME.tar.gz $DIR
+
+--
+
+$file = "script.sh";
+
+   $params = array('filename', 'path');
+   $param = ' '.implode(' ', $params);
+    if (file_exists($file)) {
+       chmod($file, "go+x");
+       $string = shell_exec('sh '.$file.$param);
+       $data = explode("\n", $string);
+       //Nome: filename - Diretório: path
+       echo 'Nome: '. $data[0] . ' - Diretório: ' . $data[1]; 
+    } else {
+      echo 'arquivo  não existe';
+    }
+--
