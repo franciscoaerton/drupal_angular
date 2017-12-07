84b82b28d0e45477d9674e41e32e174e46485922
franciscoaerton <franciscoaerton@gmail.com>
2017-12-07 09:14:57 -0200

    Update sync_server_manegement.php

--- sync_server_manegement.php
+++ sync_server_manegement.php
@@ -16,7 +16,7 @@ $session_id = ispconfig_api_login();
 $parameters = array(
   'sys_userid' => 0,
   'sys_groupid' => 0
- );
+);
  $result = ispconfig_api('client_get_sites_by_user', $parameters);
 //kpr ($result);
 //kpr ($session_id);
@@ -24,25 +24,29 @@ $end = ispconfig_api_logout($session_id);
 
 foreach ($result as $key => $value) {
 //print_r($value);
-  echo '<tr> <td>ordem'.$key.'</td> <td>'.$value['domain_id'],'</td> <td>'.$value['domain'].'</td> <td>'.$value['document_root'].'</td> <td>link</td></tr>';
+  echo '<tr>
+          <td>ordem'.$key.'</td>
+          <td>'.$value['domain_id']'</td>
+          <td>'.$value['domain'].'</td>
+          <td>'.$value['document_root'].'</td>
+          <td>link</td>
+        </tr>';
 }
  // Endpoint URL.
-   $url = 'https://painel.ativos.online/rest_view_06122017/rest01.json';
-   $ch = curl_init();
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-   curl_setopt($ch, CURLOPT_URL, $url);
+$url = 'https://painel.ativos.online/rest_view_06122017/rest01.json';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
    // Set authentication details.
-   curl_setopt($ch, CURLOPT_USERPWD, "admin_webmaster:pW_webmaster@02157781");
+curl_setopt($ch, CURLOPT_USERPWD, "admin_webmaster:pW_webmaster@02157781");
    // Fetch the results.
-   $result = curl_exec($ch);
-   curl_close($ch); 
+$result = curl_exec($ch);
+curl_close($ch); 
 
 $obj = json_decode($result );
 print_r( $obj); // 12345
 
 kpr($obj);
-
-
 ?>
     </tbody>
   </table>
