e9bc745c196deb55ffacc1bcb5e44ef931e7f08e
franciscoaerton <franciscoaerton@gmail.com>
2017-12-18 13:46:57 -0200

    Update sh_drupal_001.php

--- sh_drupal_001.php
+++ sh_drupal_001.php
@@ -1,9 +1,15 @@
 <?php
 
-$_biblioteca_sh = ; //sites/default/files/private arquivo para ser executado
+$_biblioteca_sh = "/sites/default/files/private/"
 $_diretorio_web = ; //diretorio raiz da instalação
 $_application_ftp = ; //endereço ftp da applicação -- poder ser local em drupal
 $_tar_folder = ; // nome da pasta criada que será expluida apos a instalação
+
+
+//$_biblioteca_sh = ; //sites/default/files/private arquivo para ser executado
+//$_diretorio_web = ; //diretorio raiz da instalação
+//$_application_ftp = ; //endereço ftp da applicação -- poder ser local em drupal
+//$_tar_folder = ; // nome da pasta criada que será expluida apos a instalação
 
 shell_exec('sh $_biblioteca_sh $_diretorio_web $_application_ftp $_tar_folder');
 
